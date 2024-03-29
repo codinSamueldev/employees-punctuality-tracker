7e049a763601f9e57588b718f7df9a51b3542572
Samuel Henao <codinsamueldev@gmail.com>
2024-03-29 11:41:55 -0500

    fix: remove functions to fix db connection bug

--- sqlite_connection.py
+++ sqlite_connection.py
@@ -1,12 +1,4 @@
 import sqlite3
 
-def db_connection() -> sqlite3.Connection:
-        con = sqlite3.connect('employees.db')
-        
-        return con
-    
-def db_cursor() -> sqlite3.Cursor:
-    con = db_connection()
-    cursor = con.cursor()
-
-    return cursor
+con = sqlite3.connect('employees.db')
+cursor = con.cursor()
