830fe73abd8aca8d34a35f89f481922680cd14c0
Samuel Henao <codinsamueldev@gmail.com>
2024-03-29 00:11:38 -0500

    feat: improve user experience by just typing necessary info

--- insert_data.py
+++ insert_data.py
@@ -29,9 +29,21 @@ def insert_into_tracker(data_array: list) -> bool:
         
 
 if __name__ == "__main__":
-    basic_data = [(5, "28/03/2024", "05:55 PM", "10:00 PM")]
+    basic_data = []
 
-    insert_into_tracker(basic_data)
+    employee_id = int(input("Type employee id: "))
+    basic_data.append(employee_id)
+
+    current_date = input("Type current date with the following format (DD/MM/YY): ").strip()
+    basic_data.append(current_date)
+
+    entry_hour = input("Type employee today's entry hour with the following format (10:05 AM, or, 05:41 PM): ").strip()
+    basic_data.append(entry_hour)
+
+    exit_hour = input("Type employee today's exit hour with the following format (10:05 AM, or, 05:41 PM): ").strip()
+    basic_data.append(exit_hour)
+
+    insert_into_tracker([tuple(basic_data)])
 
     ####################################################
 
