a396a7ae3bd3bc15b02dfedf0e65d4345de4504e
Samuel Henao <codinsamueldev@gmail.com>
2024-03-30 19:29:03 -0500

    create src directory and move src code

---
+++ src/retrieve_data/services/useful_services.py
@@ -0,0 +1,7 @@
+class RetrievalServices:
+    def __init__(self) -> None:
+        pass
+
+    def clean_and_frame_data(self, response: list) -> print:
+        for res in response:
+            pass
