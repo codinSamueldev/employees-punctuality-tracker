7fe90cb715f62fb7d6018f5840862b4897394e75
Samuel Henao <codinsamueldev@gmail.com>
2024-04-07 19:21:04 -0500

    fix: use aboslute paths to fix operationalError bug

--- src/db/sqlite_connection.py
+++ src/db/sqlite_connection.py
@@ -1,4 +1,10 @@
+import os.path
 import sqlite3
 
-con = sqlite3.connect('employees.db')
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "employees.db")
+
+
+con = sqlite3.connect(db_path)
 cursor = con.cursor()
