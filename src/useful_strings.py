3850a65b7441b89128083a8487b1d4669c614e52
Samuel Henao <codinsamueldev@gmail.com>
2024-04-10 19:59:25 -0500

    new: create variables to store strings that contains useful info to user

---
+++ src/useful_strings.py
@@ -0,0 +1,18 @@
+INITIAL_WELCOME_OPTIONS = """
+        Hi there! thanks for using this app. Please tell me what do you want to do:
+            1) Retrieve information such as schedules, tardiness, employees...
+            2) Input new data.
+            3) Exit.
+        
+        Type your option (1, 2, 3) and hit enter: """
+
+RETRIEVAL_OPTIONS = """
+        Roger that! Please tell me what kind of data would you like to know:
+            1) Employees.
+            2) Schedules.
+            3) Punctuality tracker.
+        
+        Type your option (1, 2, 3) and hit enter: """
+
+GOODBYE = """
+        Alright, alright, alright! Thanks for using us :D """
