96738e0cd62d44220768df68ce404ba9b8d2a7df
Samuel Henao <codinsamueldev@gmail.com>
2024-04-10 19:34:08 -0500

    test schedules script

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from db.sqlite_connection import con, cursor
 from retrieve_data.employees.table import RetrieveEmployeesTable
+from retrieve_data.schedules.table import RetrieveSchedulesTable
 
 
 
 if __name__ == '__main__':
-    RetrieveEmployeesTable(con=con, cursor=cursor).retrieve_all_employees()
+    # RetrieveEmployeesTable(con=con, cursor=cursor).retrieve_all_employees()
+    RetrieveSchedulesTable(con=con, cursor=cursor).retrieve_employees_schedules()
